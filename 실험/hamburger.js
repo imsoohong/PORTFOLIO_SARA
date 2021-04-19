a877c76780f5220d5355c64268e02aa05f91ce0f
imsoohong <75714615+imsoohong@users.noreply.github.com>
2021-04-19 21:20:37 +0900

    code modify

--- 실험/hamburger.js
+++ 실험/hamburger.js
@@ -11,18 +11,22 @@ navbarToggleBtn.addEventListener('click', () => {
     // console.log('토글 버튼 실행');
 });
 
+/* 남자 메뉴 */
 const manMenu = document.querySelector('.man__menu');
-const womanMenu = document.querySelector('.woman__menu');
-
 const manList = document.querySelector('.man__list');
+
+/* 여자 메뉴 */
+const womanMenu = document.querySelector('.woman__menu');
 const womanList = document.querySelector('.woman__list');
 
+/* 남자 메뉴 클릭 */
 manMenu.addEventListener('click', () => {
     // console.log('남자 메뉴 클릭');
     manList.classList.toggle('open');
     womanList.classList.remove('open');
 });
 
+/* 여자 메뉴 클릭 */
 womanMenu.addEventListener('click', () => {
     // console.log('여자 메뉴 클릭');
     womanList.classList.toggle('open');
